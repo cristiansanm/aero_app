d0e442e613e5361dc7a2452cc2c488c9a2f6dc56
cristiansanm <krissanz13@gmail.com>
2021-02-24 00:13:57 +0100

    second commit

--- src/Controller/BasesController.php
+++ src/Controller/BasesController.php
@@ -2,28 +2,99 @@
 
 namespace App\Controller;
 
+use App\Entity\Bases;
+use App\Form\BasesType;
 use App\Repository\BasesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/bases")
+ */
 class BasesController extends AbstractController
 {
     /**
-     * @Route("/bases", name="bases")
+     * @Route("/", name="bases_principal")
      */
-    public function index(): Response
-    {
-        return $this->render('bases/index.html.twig', [
-            'controller_name' => 'BasesController',
-        ]);
+    public function principal(){
+        return $this->render('bases/principal_bases.html.twig');
     }
     /**
-     * @Route("/bases/lista", name="lista_bases")
+     * @Route("/index", name="bases_index", methods={"GET"})
      */
-    public function lista(BasesRepository $basesRepository){
-        $bases = $basesRepository->findAll();
-        return $this->render('bases/listado_bases.html.twig',
-        ['bases' => $bases]);
+    public function index(BasesRepository $basesRepository): Response
+    {
+        return $this->render('bases/index.html.twig', [
+            'bases' => $basesRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="bases_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $basis = new Bases();
+        $form = $this->createForm(BasesType::class, $basis);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($basis);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('bases_index');
+        }
+
+        return $this->render('bases/new.html.twig', [
+            'basis' => $basis,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="bases_show", methods={"GET"})
+     */
+    public function show(Bases $basis): Response
+    {
+        return $this->render('bases/show.html.twig', [
+            'basis' => $basis,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="bases_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Bases $basis): Response
+    {
+        $form = $this->createForm(BasesType::class, $basis);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('bases_index');
+        }
+
+        return $this->render('bases/edit.html.twig', [
+            'basis' => $basis,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="bases_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Bases $basis): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$basis->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($basis);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('bases_index');
     }
 }
